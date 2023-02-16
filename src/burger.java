1ffa460c5b36a6b616464b537ef77d6ed7e36105
Valeriia <valeriia.matsyk@gmail.com>
2023-02-16 23:13:42 +0200

    burger

--- src/burger.java
+++ src/burger.java
@@ -4,6 +4,7 @@ public class burger {
     String cheese;
     String greens;
     String mayonnaise;
+
     public static void main(String[] args) {
         burger standart = new burger();
         standart.bun = "bun";
@@ -12,6 +13,21 @@ public class burger {
         standart.greens = "greens";
         standart.mayonnaise = "mayonnaise";
 
-        System.out.println("Состав стандартного бургера: " + standart.bun + ", " + standart.meat + ", " + standart.cheese + ", " + standart.greens  + ", " + standart.mayonnaise);
+        System.out.println("Состав стандартного бургера: " + standart.bun + ", " + standart.meat + ", " + standart.cheese + ", " + standart.greens + ", " + standart.mayonnaise);
+        burger diet = new burger();
+        diet.bun = "bun";
+        diet.meat = "meat";
+        diet.cheese = "cheese";
+        diet.greens = "greens";
+
+        System.out.println("Состав диетического бургера: " + diet.bun + ", " + diet.meat + ", " + diet.cheese + ", " + diet.greens);
+        burger kingsize = new burger();
+        kingsize.bun = "bun";
+        kingsize.meat = "meat";
+        kingsize.cheese = "cheese";
+        kingsize.greens = "greens";
+        kingsize.mayonnaise = "double mayonnaise";
+
+        System.out.println("Состав двойного бургера: " + kingsize.bun + ", " + kingsize.meat + ", " + kingsize.cheese + ", " + kingsize.greens + ", " + kingsize.mayonnaise);
     }
 }
